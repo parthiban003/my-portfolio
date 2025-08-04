d8234ac32a22530aec42cb165dd2d1f170bdf890
parthiban003 <parthiban2912003@gmail.com>
2025-08-04 17:28:54 +0530

    pending works completed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -47,7 +47,7 @@ export const Skills = () => {
               key={key}
               onClick={() => setActiveCategory(category)}
               className={cn(
-                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                "px-5 py-2 rounded-full transition-colors duration-300 capitalize cursor-pointer",
                 activeCategory === category
                   ? "bg-primary text-primary-foreground"
                   : "bg-secondary/70 text-forefround hover:bd-secondary"
